4a8af0fa28bc40f9b9f9bccb71b7d721250e8079
이유진 <iyujin@iyujin-ui-noteubug.local>
2024-05-14 12:39:36 +0900

    Intitial Commit

---
+++ helloWeather/View/WeatherDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherDetailViewController.swift
+//  helloWeather
+//
+//  Created by 이유진 on 5/14/24.
+//
+
+import UIKit
+
+class WeatherDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
