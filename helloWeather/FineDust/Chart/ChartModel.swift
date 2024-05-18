03eba6ef75d119b66919db6596121e426c4dab7f
kimtaedam <54749691+incoverlet@users.noreply.github.com>
2024-05-18 22:53:08 +0900

    [Fix] ChartView 수정

--- helloWeather/FineDust/Chart/ChartModel.swift
+++ helloWeather/FineDust/Chart/ChartModel.swift
@@ -25,9 +25,7 @@ public struct ChartView: View {
                 legend: String? = nil,
                 style: ChartStyle2 = Styles2.lineChartStyleOne,
                 valueSpecifier: String? = "%.1f",
-                legendSpecifier: String? = "%.1f",
-                hideXAxis: Bool = false,
-                hideYAxis: Bool = false) {
+                legendSpecifier: String? = "%.1f") {
         
         self.data = ChartData(points: data)
         self.title = nil
@@ -61,13 +59,13 @@ public struct ChartView: View {
                         if(self.showLegend){
                         }
                         Line2(data: self.data,
-                             frame: .constant(CGRect(x: 0, y: 0, width: reader.frame(in: .local).width - 30, height: reader.frame(in: .local).height + 25)),
-                             touchLocation: self.$indicatorLocation,
-                             showIndicator: self.$hideHorizontalLines,
-                             minDataValue: .constant(nil),
-                             maxDataValue: .constant(nil),
-                             showBackground: false,
-                             gradient: self.style.gradientColor
+                              frame: .constant(CGRect(x: 0, y: 0, width: reader.frame(in: .local).width - 30, height: reader.frame(in: .local).height + 25)),
+                              touchLocation: self.$indicatorLocation,
+                              showIndicator: self.$hideHorizontalLines,
+                              minDataValue: .constant(nil),
+                              maxDataValue: .constant(nil),
+                              showBackground: false,
+                              gradient: self.style.gradientColor
                         )
                         .offset(x: 30, y: 0)
                         .onAppear(){
@@ -85,17 +83,104 @@ public struct ChartView: View {
                 }
                 .frame(width: geometry.frame(in: .local).size.width, height: 240)
                 .gesture(DragGesture()
-                .onChanged({ value in
-                    self.dragLocation = value.location
-                    self.indicatorLocation = CGPoint(x: max(value.location.x-30,0), y: 32)
-                    self.opacity = 1
-                    self.closestPoint = self.getClosestDataPoint(toPoint: value.location, width: geometry.frame(in: .local).size.width-30, height: 240)
-                    self.hideHorizontalLines = true
-                })
-                    .onEnded({ value in
-                        self.opacity = 0
-                        self.hideHorizontalLines = false
+                    .onChanged({ value in
+                        self.dragLocation = value.location
+                        self.indicatorLocation = CGPoint(x: max(value.location.x-30,0), y: 32)
+                        self.opacity = 1
+                        self.closestPoint = self.getClosestDataPoint(toPoint: value.location, width: geometry.frame(in: .local).size.width-30, height: 240)
+                        self.hideHorizontalLines = true
                     })
+                        .onEnded({ value in
+                            self.opacity = 0
+                            self.hideHorizontalLines = false
+                        })
+                )
+            }
+        }
+    }
+    
+}
+
+public struct ChartView2: View {
+    @ObservedObject var data: ChartData
+    public var title: String?
+    public var legend: String?
+    public var style: ChartStyle2
+    public var darkModeStyle: ChartStyle2
+    public var valueSpecifier: String
+    public var legendSpecifier: String
+    
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    @State private var showLegend = false
+    @State private var dragLocation:CGPoint = .zero
+    @State private var indicatorLocation:CGPoint = .zero
+    @State private var closestPoint: CGPoint = .zero
+    @State private var opacity:Double = 0
+    @State private var currentDataNumber: Double = 0
+    @State private var hideHorizontalLines: Bool = false
+    
+    public init(data: [Double],
+                title: String? = nil,
+                legend: String? = nil,
+                style: ChartStyle2 = Styles2.lineChartStyleOne,
+                valueSpecifier: String? = "%.1f",
+                legendSpecifier: String? = "%.1f") {
+        
+        self.data = ChartData(points: data)
+        self.title = nil
+        self.legend = legend
+        self.style = style
+        self.valueSpecifier = valueSpecifier!
+        self.legendSpecifier = legendSpecifier!
+        self.darkModeStyle = style.darkModeStyle != nil ? style.darkModeStyle! : Styles2.lineViewDarkMode
+    }
+    
+    func getClosestDataPoint(toPoint: CGPoint, width:CGFloat, height: CGFloat) -> CGPoint {
+        let points = self.data.onlyPoints()
+        let stepWidth: CGFloat = width / CGFloat(points.count-1)
+        let stepHeight: CGFloat = height / CGFloat(points.max()! + points.min()!)
+        
+        let index:Int = Int(floor((toPoint.x-15)/stepWidth))
+        if (index >= 0 && index < points.count){
+            self.currentDataNumber = points[index]
+            return CGPoint(x: CGFloat(index)*stepWidth, y: CGFloat(points[index])*stepHeight)
+        }
+        return .zero
+    }
+    
+    public var body: some View {
+        GeometryReader{ geometry in
+            VStack(alignment: .leading, spacing: 8) {
+                ZStack{
+                    GeometryReader{ reader in
+                        Rectangle()
+                            .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.backgroundColor : self.style.backgroundColor)
+                        if(self.showLegend){
+                        }
+                        
+                    }
+                    .frame(width: geometry.frame(in: .local).size.width, height: 240)
+                    .offset(x: 0, y: 40)
+                    MagnifierRect(currentNumber: self.$currentDataNumber, valueSpecifier: self.valueSpecifier)
+                        .opacity(self.opacity)
+                        .offset(x: self.dragLocation.x - geometry.frame(in: .local).size.width/2, y: 36)
+                }
+                .frame(width: geometry.frame(in: .local).size.width, height: 240)
+                .gesture(DragGesture()
+                    .onChanged({ value in
+                        let lowerBound = geometry.frame(in: .local).size.width * 0.2
+                        let upperBound = geometry.frame(in: .local).size.width * 0.8
+                        if value.location.x >= lowerBound && value.location.x <= upperBound {
+                            self.dragLocation = value.location
+                            self.indicatorLocation = CGPoint(x: max(value.location.x - 30, 0), y: 32)
+                            self.opacity = 1
+                            self.closestPoint = self.getClosestDataPoint(toPoint: value.location, width: geometry.frame(in: .local).size.width - 30, height: 240)
+                            self.hideHorizontalLines = true
+                        }                })
+                        .onEnded({ value in
+                            self.opacity = 0
+                            self.hideHorizontalLines = false
+                        })
                 )
             }
         }
@@ -107,6 +192,7 @@ struct LineView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ChartView(data: [282.502, 284.495, 283.51, 285.019, 285.197, 286.118, 288.737, 288.455, 289.391, 287.691, 285.878, 286.46, 286.252, 284.652, 284.129, 284.188], title: "Full chart", style: Styles2.lineChartStyleOne)
+            
             
         }
     }
--- helloWeather/FineDust/FineDustModel.swift
+++ helloWeather/FineDust/FineDustModel.swift
@@ -90,22 +90,98 @@ struct LineChartView: View {
                         }
                     Spacer()
                 }
-                ScrollView(.horizontal){
-                    ChartView(data: [282.502, 284.495, 283.51, 285.019, 285.197, 286.118, 288.737, 288.455, 289.391, 287.691, 285.878, 286.46, 286.252, 284.652, 284.129, 284.188], title: "Full chart", style: Styles2.lineChartStyleOne)
-                        .frame(width: 700)
+            }
+            ZStack{
+                
+                ScrollView(.horizontal, showsIndicators: false){
+                    ZStack{
+                        //ㅇㅇ
+                        VStack{
+                            ChartView(data: [282.502, 284.495, 283.51, 285.019, 285.197, 286.118, 288.737, 288.455, 289.391, 287.691, 285.878, 286.46, 286.252], title: "Full chart", style: Styles2.lineChartStyleOne)
+                                .frame(width: 800,height: 300, alignment: .center)
+                            Spacer()
+                            HStack{
+                                Spacer()
+                                Text("지금")
+                                    .bold()
+                                Spacer()
+                                ForEach(3..<24) { i in
+                                    if i % 3 == 0 {
+                                        Text("\(i)")
+                                        Spacer()
+                                    }
+                                }
+                                Text("내일")
+                                    .font(.system(size: 15))
+                                    .foregroundColor(.white)
+                                    .padding(5)
+                                    .background(Color.black)
+                                    .cornerRadius(10)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color.black, lineWidth: 2)
+                                    )
+                                Spacer()
+                                ForEach(3..<24) { i in
+                                    if i % 3 == 0 {
+                                        Text("\(i)")
+                                        Spacer()
+                                    }
+                                }
+                                Text("모래")
+                                    .font(.system(size: 15))
+                                    .foregroundColor(.white)
+                                    .padding(5)
+                                    .background(Color.black)
+                                    .cornerRadius(10)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color.black, lineWidth: 2)
+                                    )
+                            }
+                            
+                        }
+                        //중간선
+                        HStack{
+                            Spacer()
+                            VStack{
+                                ForEach(0..<12) { i in
+                                    Text("|")
+                                        .bold()
+                                        .foregroundColor(.black)
+                                }
+                            }
+                            Spacer()
+                        }.padding(.leading, 20)
+                        HStack{
+                            ForEach(0..<20) { _ in
+                                Spacer()
+                            }
+                            VStack{
+                                ForEach(0..<12) { i in
+                                    Text("|")
+                                        .bold()
+                                        .foregroundColor(.black)
+                                        .padding(.leading, 460)
+                                }
+                            }
+                            Spacer()
+                        }
+                    }
+//                    .contentShape(Rectangle()) // Makes the entire area tappable
+//                    .gesture(DragGesture(minimumDistance: 0)
+//                        .onChanged { _ in } // Handle touch events without allowing scroll
+//                    )
+                    
                 }
-                ChartView(data: [282.502, 284.495, 283.51, 285.019, 285.197, 286.118, 288.737, 288.455, 289.391, 287.691, 285.878, 286.46, 286.252, 284.652, 284.129, 284.188], title: "Full chart", style: Styles2.lineChartStyleOne)
-                    //.frame(height: 300)
+                
                 
             }
-            
-            
-    
-            //            FineListView(viewModel: fineListViewModel)
-            //            ValueList(viewModel: fineListViewModel)
-        }
-        //.frame(height: 1500)
-        
+            FineListView(viewModel: fineListViewModel)
+                .padding(.bottom, 50)
+            ValueList(viewModel: fineListViewModel)
+            Spacer()
+        }.frame(height: 800)
     }
 }
 #Preview {
